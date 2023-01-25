1be95f28a939fea5755fa61f95e0d3dd69c943e2
martinpcs21 <chuecosmartin@gmail.com>
2023-01-25 22:16:51 +0100

    Trying to format background

--- src/pages/table3.tsx
+++ src/pages/table3.tsx
@@ -5,7 +5,19 @@ import json from '../data/sfa_easy.json'
 //nested data is ok, see accessorKeys in ColumnDef below
 const data = Object.values(json)
 
+
+
+
 const Example = () => {
+
+  const colorMapPercent = {
+    0: 'rgba(255, 0, 0, 0.5)',
+    0.25: 'rgba(255, 0, 0, 0.5)',
+    0.5: 'rgba(255, 0, 0, 0.5)',
+    0.75: 'rgba(255, 0, 0, 0.5)',
+    1: 'rgba(255, 0, 0, 0.5)',
+  }
+
   //should be memoized or stable
   const columns = useMemo(
     () => [
@@ -20,18 +32,26 @@ const Example = () => {
       {
         header: "Potential",
         accessorKey: "Potential",
-        muiTableBodyCellProps: ({ cell }) => ({
-          sx: {
-            backgroundColor:
-              cell.getValue<number>() > 0.5
-                ? 'rgba(22, 184, 44, 0.5)'
-                : undefined,
-            fontWeight:
-              cell.column.id === 'age' && cell.getValue<number>() > 40
-                ? '700'
-                : '400'
-          },
-        }),
+        Cell: ({ cell }) => {
+          if (cell.getValue<number>() > 0.5) {
+            <p style ={{backgroundColor: 'rgba(255, 0, 0, 0.5)'}}> {cell.getValue<number>()} </p>
+          }
+        }
+        // muiTableBodyCellProps: ({ cell }) => ({
+        //   styleOverrides: {
+        //     backgroundColor: "red",
+        //   }
+          // sx: {
+          //   backgroundColor:
+          //     cell.getValue<number>() > 0.5
+          //       ? 'rgba(22, 184, 44, 0.5)'
+          //       : 'rgba(255, 0, 0, 0.5)',
+          //   fontWeight:
+          //     cell.column.id === 'age' && cell.getValue<number>() > 40
+          //       ? '700'
+          //       : '400'
+          // },
+        // }),
       },
       {
         header: "TargetPrice",
@@ -60,6 +80,8 @@ const Example = () => {
     ],
     [],
   );
+
+  
 
   return <MaterialReactTable 
     columns={columns} 
